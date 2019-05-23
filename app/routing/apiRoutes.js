2e87297dee7c582e88d6b7047f26ef9bf6d2371d
Kunal <patelkr07@gmail.com>
2019-05-22 19:26:10 -0500

    fixing get and post routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+});
+
+app.get("/api/friends/:friend", function(req, res) {
+    var chosen = req.params.friend;
+
+    console.log(chosen);
+
+    for (var i= 0; i < friends.length; i++) {
+        if (friend === friends[i].routeName) {
+            return res.json(friends[i]);
+        }
+    }
+
+    return res.send("No friends found");
+});
+
+
+app.post("/api/friends", function (req, res) {
+    var newFriend = req.body;
+
+    console.log(newFriend);
+
+    friends.push(newFriend);
+
+    res.json(newFriend);
+
+    res.send(body)
+});
